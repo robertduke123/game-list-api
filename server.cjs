6098ea46cbe5ecc75ed0e1668018b62205baef37
Robert Duke <robduke123@gmail.com>
2023-09-14 13:31:38 +1200

    testing fixes on frontend

--- server.cjs
+++ server.cjs
@@ -6,7 +6,7 @@ const knex = require('knex')
 const fetch = (...args) => 
     import('node-fetch').then(({default: fetch}) => fetch(...args))
 
-import express from 'express'
+// import express from 'express'
 
 
 const db = knex({
@@ -32,6 +32,7 @@ const db = knex({
 //   }
 // });
 
+db.select('*').from('users').then(console.log)
 
 const app = express()
 app.use(bodyparser.json())
@@ -128,6 +129,7 @@ app.get('/profile/:id', (req, res) => {
     .catch(err => res.status(400).json('error getting user'))
 })
 
+// const API_KEY = 'fdeb3527d9mshd0491bd056688a1p1e8abajsn07db021ba953'
 
 app.put('/search', (req, res) => {
     const {itemSearch} = req.body
@@ -147,6 +149,19 @@ app.put('/search', (req, res) => {
     .then(json => res.json(json))
 	.catch(err => console.error('error:' + err));
     
+
+    // fetch(`https://rawg-video-games-database.p.rapidapi.com/games/${itemSearch}?key=a8d817fa172443748735ff2d10862681`, {
+    //       method: 'GET',
+    //       headers: {
+    //         'X-RapidAPI-Key': '48bcd47c97msh29aeb9d40c8bed9p1b117bjsn539a69073325',
+    //         'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'            
+    //         }
+    //         })
+    //         .then(response => response.json())            
+    //         .then(data => {
+    //             res.json(data)
+    //             console.log(data);
+    //         })
 })
 
 app.put('/pers', (req, res) => {
@@ -255,7 +270,11 @@ app.put('/select', (req, res) => {
         .then(response => {
            res.json(response)
         })  
-    )    
+    )
+
+
+    
+    
 })
 
 app.put('/log_delete', (req, res) => {
